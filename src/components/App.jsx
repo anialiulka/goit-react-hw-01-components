f6a0ca654a03bf6b002fbe5de319757522f83b1f
ania.liulka <anialiulka@gmail.com>
2023-07-11 12:37:29 -0400

    Finished task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
+
+const { username, tag, location, avatar, stats } = user;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
     </div>
   );
 };
